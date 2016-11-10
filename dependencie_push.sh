6b44d7cf5fb1dde5d68ff2d1c0c05a94631ba1f9
root <root@takinosh5.mylabserver.com>
2016-11-10 20:10:58 +0000

    added master script

--- dependencie_push.sh
+++ dependencie_push.sh
@@ -8,6 +8,24 @@ fi
 USER=$1
 cp /etc/hosts hosts
 echo -e  "$2    centos-master\n$3    centos-minion1\n$4    centos-minion2\n$5    centos-minion3\n" >> hosts
+#master
 scp hosts $USER@$2:/tmp/hosts
 scp install_packages.sh $USER@$2:/home/$1/install_packages.sh
 ssh -t $USER@$2 'su - root -c  "chmod +x /home/'$1'/install_packages.sh;sh /home/'$1'/install_packages.sh;"'
+
+#minion 1
+scp hosts $USER@$3:/tmp/hosts
+scp install_packages.sh $USER@$3:/home/$1/install_packages.sh
+ssh -t $USER@$3 'su - root -c  "chmod +x /home/'$1'/install_packages.sh;sh /home/'$1'/install_packages.sh;"'
+	
+#minion 2
+scp hosts $USER@$4:/tmp/hosts
+scp install_packages.sh $USER@$4:/home/$1/install_packages.sh
+ssh -t $USER@$4 'su - root -c  "chmod +x /home/'$1'/install_packages.sh;sh /home/'$1'/install_packages.sh;"'
+
+
+#minion 3
+scp hosts $USER@$5:/tmp/hosts
+scp install_packages.sh $USER@$5:/home/$1/install_packages.sh
+ssh -t $USER@$5 'su - root -c  "chmod +x /home/'$1'/install_packages.sh;sh /home/'$1'/install_packages.sh;"'
+
--- setup-keyless.sh
+++ setup-keyless.sh
@@ -3,10 +3,10 @@ if [[ $# -ne 5 ]];then
 	echo $0: usage: ./setup-keyless.sh username 192.168.1.5 192.168.1.6 192.168.1.7 192.168.1.8
 	exit 1
 fi
-]
 USER=$1
 cd ..
 echo "create authentication ssh-keygen keys on localhost"
+echo "will set up keys on $2, $3, $4, $5"
 ssh-keygen -t rsa
 echo "create .ssh directory on remote machines"
 ssh $USER@$2 mkdir -p .ssh
@@ -19,8 +19,12 @@ cat .ssh/id_rsa.pub | ssh $USER@$3 'cat >> .ssh/authorized_keys'
 cat .ssh/id_rsa.pub | ssh $USER@$4 'cat >> .ssh/authorized_keys'
 cat .ssh/id_rsa.pub | ssh $USER@$5 'cat >> .ssh/authorized_keys'
 echo "set permissions on authorized_keys folder"
-ssh $USER@$1 "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
 ssh $USER@$2 "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
 ssh $USER@$3 "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
 ssh $USER@$4 "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
+ssh $USER@$5 "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
 echo "test logon on remote machines"
+ssh $USER@$2 hostnamectl
+ssh $USER@$3 hostnamectl
+ssh $USER@$4 hostnamectl
+ssh $USER@$5 hostnamectl
