4f0505edc640c5e80e0db207b9e011a093aa52a9
Tosin Akinosho <tosin.akinosho@gmail.com>
2018-11-07 15:37:01 +0000

    Configure fireall and remote workers

---
+++ ubuntu/configure_firewall_ports.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+if [[ -z $1 ]]; then
+    echo "Please pass node type"
+    echo "USAGE: ./configure_firewall_ports.sh master/worker"
+    exit 1
+fi
+
+echo -e "
+\e[1;4mKubernetes firewalls \e[0m
+\e[32mMaster Nodes: \e[0m
+\e[1mTCP \e[0m    6443*    Kubernetes API server
+\e[1mTCP \e[0m    2379-2380   etcd server client API
+\e[1mTCP \e[0m    10250   Kubelet API
+\e[1mTCP \e[0m    10251   kube-scheduler
+\e[1mTCP \e[0m    10252   kube-controller-manager
+\e[1mTCP \e[0m    10255   Read-Only Kubelet API
+
+\e[32mWorker nodes: \e[0m
+\e[1mTCP \e[0m   10250    Kubelet API
+\e[1mTCP \e[0m    10255    Read-only Kubelet API
+\e[1mTCP \e[0m    30000-32767    NodePort Services"
+
+if [[ $1 == "master" ]]; then
+    echo "Configuring Master Nodes firewall"
+    sudo ufw status | grep inactive && sudo ufw enable || exit $?
+    sudo ufw allow from any to any port 6443 proto tcp
+    sudo ufw allow from any to any port 2379:2380 proto tcp
+    sudo ufw allow from any to any port 10250 proto tcp
+    sudo ufw allow from any to any port 10251 proto tcp
+    sudo ufw allow from any to any port 10252 proto tcp
+    sudo ufw allow from any to any port 10255 proto tcp
+    sudo ufw status verbose
+elif [[ $1 == "worker" ]]; then
+    echo "Configuring worker Nodes firewall"
+    sudo ufw status | grep inactive && sudo ufw enable || exit $?
+    sudo ufw allow from any to any port 10250 proto tcp
+    sudo ufw allow from any to any port 10255 proto tcp
+    sudo ufw allow from any to any port 30000:32767 proto tcp
+    sudo ufw status verbose
+else
+    echo -e "\e[41mInvalid flag passed. Use master/worker flag \e[0m"
+    exit 1
+fi
+
---
+++ ubuntu/configure_workers.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+    echo "Please see usage"
+    echo  "USAGE: ./configure_workers.sh username"
+    echo  "USAGE EXAMPLE: ./configure_workers.sh admin"
+    exit $1
+fi
+USERNAME=$1
+
+#THANKS: https://www.linuxjournal.com/content/validating-ip-address-bash-script
+function valid_ip()
+{
+    local  ip=$1
+    local  stat=1
+
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+    fi
+    return $stat
+}
+
+COMMANDTOUSE=$(cat ~/kubeadminit.log | grep -i "kubeadm join")
+echo -e '#!/bin/bash \nsudo '${COMMANDTOUSE}'' >  /tmp/addworker.sh
+
+WORKERSFILE=$(find ~ -name  workers)
+CHECKIPS=$(cat $WORKERSFILE)
+for  worker in $CHECKIPS
+do
+    if valid_ip $worker; then
+        echo "worker: $worker"
+        scp ./ubuntu/configure_docker.sh ${USERNAME}@${worker}:/tmp/configure_docker.sh
+        scp ./ubuntu/kubeadmin.sh ${USERNAME}@${worker}:/tmp/kubeadmin.sh
+        ssh  -o PasswordAuthentication=no -o ConnectTimeout=10 -t  ${USERNAME}@${worker} "sh /tmp/configure_docker.sh" || exit $?
+        ssh  -o PasswordAuthentication=no -o ConnectTimeout=10 -t  ${USERNAME}@${worker} "sh /tmp/kubeadmin.sh" || exit $?
+        scp ./ubuntu/configure_firewall_ports.sh ${USERNAME}@${worker}:/tmp/configure_firewall_ports.sh
+        ssh  -o PasswordAuthentication=no -o ConnectTimeout=10 -t  ${USERNAME}@${worker} "sh /tmp/configure_firewall_ports.sh worker" || exit $?
+        scp  /tmp/addworker.sh ${USERNAME}@${worker}:/tmp/addworker.sh
+        ssh  -o PasswordAuthentication=no -o ConnectTimeout=10 -t  ${USERNAME}@${worker} "sh /tmp/addworker.sh" || exit $?
+    fi
+done
+
